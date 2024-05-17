f0c0e348596ff564983b132e561de6ebc99fd86a
xiaoyu <3157575603@qq.com>
2024-05-17 19:39:18 +0800

    删掉其他库的代码

--- src/plugin/MenuPlugin.cpp
+++ src/plugin/MenuPlugin.cpp
@@ -4,15 +4,9 @@
 #include "Command/MenuCommandRegister.h"
 #include "Event/MenuEventRegister.h"
 #include "HTTP/HttpService.h"
-// #include "HTTP/http/HttpClient.h"
 #include "ll/api/plugin/NativePlugin.h"
 #include "ll/api/plugin/RegisterHelper.h"
 #include "Log/Log.h"
-// #include "curl/curl.h"
-#include "hv/HttpClient.h"
-#include "hv/requests.h"
-
-// using namespace hv;
 
 namespace MenuPlugin
 {
